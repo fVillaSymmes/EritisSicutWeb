c874815820cd02d07d3d7b6e82b91ef97ad65ec7
Javier Sandoval <javiersandovaljunemann@gmail.com>
2024-06-21 11:36:58 -0400

    Se inaguran rutas. Se habilitan tres componentes navbar, burgermenu navitems. Primer borrador del sitio

---
+++ src/app/(general)/contacto/page.tsx
@@ -0,0 +1,12 @@
+export default function Contacto() {
+    return (
+        <main className='p-24 flex flex-col items-center '>
+            <h1>Contacto</h1>
+            <ul>
+                <li>Horacio Ferro</li>
+                <li>Felipe Villarubia</li>
+                <li>Javier Sandoval</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/app/(general)/mision-vision/page.tsx
@@ -0,0 +1,25 @@
+export default function MisionVision() {
+    return (
+        <main>
+            <h1>Que Hacemos</h1>
+            <ul>
+                <li>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Beatae iusto amet repellat sed reiciendis ad, asperiores a
+                    dolore alias natus non tempora. Facere molestias rem, minus
+                    nisi explicabo aperiam id?
+                </li>
+            </ul>
+            <h1>Estatutos</h1>
+            <ul>
+                <li>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quod, ullam sed quisquam qui, voluptates nesciunt debitis
+                    voluptatibus nostrum numquam repellat reprehenderit
+                    dignissimos distinctio, praesentium asperiores! Nam
+                    doloribus saepe laboriosam et!
+                </li>
+            </ul>
+        </main>
+    )
+}
