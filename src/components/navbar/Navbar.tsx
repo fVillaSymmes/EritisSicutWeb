0d76bee578dec8061e917a25416fbb9e5da95fb4
Javier Sandoval <javiersandovaljunemann@gmail.com>
2024-08-01 14:42:29 -0400

    primer-commit-prueba

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -18,7 +18,7 @@ const navItems = [
 
 export const Navbar = () => {
   return (
-    <nav className='rounded border border-gray-200 bg-white px-2 py-2.5 shadow sm:px-4 dark:border-gray-700 dark:bg-gray-800'>
+    <nav className='rounded border border-gray-300 bg-white px-2 py-2.5 shadow sm:px-4 dark:border-gray-700 dark:bg-gray-800'>
       <div className='container mx-auto flex flex-wrap items-center justify-between'>
         <a
           href='/'
